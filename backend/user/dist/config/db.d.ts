b80f06fb9793da4a8be3db5f4c70177c6553b9f9
Himanshu <hraj808404@gmail.com>
2026-05-31 11:45:26 +0530

    chore: remove tracked dist folder and respect gitignore

--- backend/user/dist/config/db.d.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-declare const prisma: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-export declare const connectDb: () => Promise<void>;
-export default prisma;
-//# sourceMappingURL=db.d.ts.map
