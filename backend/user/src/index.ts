08cd94886a3d9c709149594c00e150bf84a17ea1
Himanshu <hraj808404@gmail.com>
2026-05-31 12:26:45 +0530

    feat: initialize Redis in the user service

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDb } from './config/db.js';
-
+import { createClient } from 'redis';
 dotenv.config();
 
-const app=express();
-const PORT=process.env.PORT;
+export const redisClient = createClient({
+  url: process.env.REDIS_URL as string,
+});
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
 
 app.use(express.json());
 
 app.listen(PORT, async () => {
   console.log(`[User Service] Server is running on port ${PORT}`);
-  await connectDb(); 
+  await connectDb();
 });
